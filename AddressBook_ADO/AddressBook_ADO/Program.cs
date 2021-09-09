a3bfab1feea6fe183af045d94b9b84343ef422ae
satyam4853 <vaishnavsatyam48532gmail.com>
2021-09-09 20:52:42 +0530

    [satyam] Add ; Welcome Message

---
+++ AddressBook_ADO/AddressBook_ADO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_ADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------Welcome To AddressBook ADO-------------");
+        }
+    }
+}
